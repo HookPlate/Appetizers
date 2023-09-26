da66764be6654cba33812d9e965bb9947a3a9b6f
HookPlate <41370249+HookPlate@users.noreply.github.com>
2023-09-26 13:20:42 +0100

    Hooks up the list view with the detail view

--- Appetizers/Screens/AppetizerListView/AppetizerListView.swift
+++ Appetizers/Screens/AppetizerListView/AppetizerListView.swift
@@ -10,22 +10,36 @@ import SwiftUI
 struct AppetizerListView: View {
     //being @state when the below fills it redraws the view.
     @StateObject var viewModel = AppetizerListViewModel()
+    @State private var isShowingDetail = false
+    @State private var selectedAppetizer: Appetizer?
     
     var body: some View {
         ZStack {
             NavigationView {
                 List(viewModel.appetizers) { appetizer in
                     AppetizerListCell(appetizer: appetizer)
+                        .onTapGesture {
+                            isShowingDetail = true
+                            selectedAppetizer = appetizer
+                        }
                 }
                 .navigationTitle("🍟 Appetizers")
             }
             .onAppear{
                 viewModel.getAppetizers()
             }
+            .blur(radius: isShowingDetail ? 20 : 0)
+            .disabled(isShowingDetail)
+            
+            if isShowingDetail {
+                AppetizerDetailView(appetizer: selectedAppetizer!, isShowingDetail: $isShowingDetail)
+            }
+            
             if viewModel.isLoading {
                 LoadingView()
             }
         }
+        
         //the isPresented version of this modifier is for use with a bool, the item one is for when an item changes.
         .alert(item: $viewModel.alertItem) { alertItem in
             Alert(title: alertItem.title, message: alertItem.message, dismissButton: alertItem.dismissButton)
