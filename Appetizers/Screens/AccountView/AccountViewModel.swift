a279ff90735d908d1c4331057be6158b14baf9c0
HookPlate <41370249+HookPlate@users.noreply.github.com>
2023-09-28 07:22:39 +0100

    A commit just before I attempt the Error challenge.

--- Appetizers/Screens/AccountView/AccountViewModel.swift
+++ Appetizers/Screens/AccountView/AccountViewModel.swift
@@ -15,4 +15,19 @@ final class AccountViewModel: ObservableObject {
     @Published var birthDate = Date()
     @Published var extraNapkins = false
     @Published var frequentRefills = false
+    
+    var isValidForm: Bool {
+        //we perform all these checks, if any of them fail we return false, if they pass we return true.
+        guard !firstName.isEmpty && !lastName.isEmpty && !email.isEmpty else {
+            
+            return false
+        }
+        
+        guard email.isValidEmail else {
+            
+            return false
+        }
+        
+        return true
+    }
 }
